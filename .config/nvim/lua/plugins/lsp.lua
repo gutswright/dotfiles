f84457ab8ae229e5026ad5054b8df86a564ef382
Guts <gutsw20@gmail.com>
2025-02-26 01:51:29 -0700

    fixed scrolling in snacks plugin

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -30,14 +30,14 @@ return {
             local servers = {
                 svelte = {},
                 tailwindcss = {},
-                -- dartls = {
-                --     settings = {
-                --         dart = {
-                --             completFunctionCalls = true,
-                --             showTodos = true,
-                --         },
-                --     },
-                -- },
+                dartls = {
+                    settings = {
+                        dart = {
+                            completFunctionCalls = true,
+                            showTodos = true,
+                        },
+                    },
+                },
                 tinymist = {
                     root_dir = function(_, bufnr)
                         return vim.fn.expand('%:p:h')
