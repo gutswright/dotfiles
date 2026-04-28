1da114f9a3ddf91270747a1912c5da10007966fe
Guts <gutsw20@gmail.com>
2026-04-27 20:00:03 -0600

    treesitter patch for python files to render correctly.

--- .config/nvim/lua/plugins/lsp_lint_and_completions/treesitter.lua
+++ .config/nvim/lua/plugins/lsp_lint_and_completions/treesitter.lua
@@ -1,11 +1,50 @@
+-- return {
+--   {
+--     'nvim-treesitter/nvim-treesitter',
+--     branch = 'main',
+--     build = ':TSUpdate',
+--     config = function()
+--       -- require('nvim-treesitter.configs').setup({
+--       require('nvim-treesitter.config').setup({
+--         ensure_installed = {
+--           'lua',
+--           'vim',
+--           'javascript',
+--           'html',
+--           'css',
+--           'python',
+--           'dockerfile',
+--           'fish',
+--           'json',
+--           'kotlin',
+--           'hcl',
+--           'markdown',
+--           'markdown_inline',
+--           'powershell',
+--           'rust',
+--           'sql',
+--           'ssh_config',
+--           'svelte',
+--           'terraform',
+--           'typescript',
+--           'typst',
+--           'zig',
+--           'cpp',
+--         },
+--         highlight = { enable = true },
+--         indent = { enable = true },
+--       })
+--     end,
+--   },
+-- }
+
 return {
   {
     'nvim-treesitter/nvim-treesitter',
-    branch = 'main',
     build = ':TSUpdate',
+    branch = 'main',
     config = function()
-      -- require('nvim-treesitter.configs').setup({
-      require('nvim-treesitter.config').setup({
+      require('nvim-treesitter').setup({
         ensure_installed = {
           'lua',
           'vim',
@@ -31,8 +70,12 @@ return {
           'zig',
           'cpp',
         },
-        highlight = { enable = true },
-        indent = { enable = true },
+      })
+
+      vim.api.nvim_create_autocmd('FileType', {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
       })
     end,
   },
